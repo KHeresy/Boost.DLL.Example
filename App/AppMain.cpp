538a0f07480b7788eb0e187d3bffed2b31b9d242
Heresy <kheresy@gmail.com>
2016-07-05 13:14:28 +0800

    Add second class, use directory can to load

--- App/AppMain.cpp
+++ App/AppMain.cpp
@@ -8,18 +8,26 @@
 
 int main(int argc, char** argv)
 {
-	#ifdef _WIN32
-	boost::filesystem::path pathDLL = "DLLa.dll";
-	#else
-	boost::filesystem::path pathDLL = "libDLLa.so";
-	#endif
-
 	// generate data
-	std::vector<double> vData = {1,2,3,4,5,6,7,8,9,0};
+	std::vector<double> vData = { 1,2,3,4,5,6,7,8,9,0 };
 
-	std::function<API_Create> funcExt = boost::dll::import_alias<API_Create>(pathDLL, "create_plugin");
-	boost::shared_ptr<CMyAPI> pModule = funcExt();
-	std::cout << "Module [" << pModule->getName() << "], result: " << pModule->compute(vData) << std::endl;
+	//
+	boost::filesystem::path pathDir = "./";
+	for (auto itFile = boost::filesystem::directory_iterator(pathDir); itFile != boost::filesystem::directory_iterator(); ++itFile)
+	{
+		if (boost::filesystem::is_regular_file(*itFile))
+		{
+			boost::filesystem::path pathFile = *itFile;
+			if (pathFile.extension() == ".dll" || pathFile.extension() == ".so")
+			{
+				std::cout << "\nTry to load " << pathFile << std::endl;
+
+				std::function<API_Create> funcExt = boost::dll::import_alias<API_Create>(pathFile, "create_plugin");
+				boost::shared_ptr<CMyAPI> pModule = funcExt();
+				std::cout << "Module [" << pModule->getName() << "], result: " << pModule->compute(vData) << std::endl;
+			}
+		}
+	}
 
 	return 0;
 }
---
+++ DLLb/DLLb.cpp
@@ -0,0 +1,44 @@
+#include "../API/MyAPI.h"
+
+#include <iostream>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/dll/alias.hpp>
+
+class CAvg : public CMyAPI
+{
+public:
+	CAvg()
+	{
+		std::cout << "CAvg constructor" << std::endl;
+	}
+
+	~CAvg()
+	{
+		std::cout << "CAvg deconstructor" << std::endl;
+	}
+
+	std::string getName() const override
+	{
+		return "CAvg";
+	}
+
+	double compute(const std::vector<double>& rData) const override
+	{
+		if (rData.size() == 0)
+			return 0.0;
+
+		double dSum = 0;
+		for (const double& dVal : rData)
+			dSum += dVal;
+
+		return dSum / rData.size();
+	}
+
+public:
+	static boost::shared_ptr<CMyAPI> create() {
+		return boost::shared_ptr<CMyAPI>( new CAvg() );
+	}
+};
+
+BOOST_DLL_ALIAS(CAvg::create, create_plugin )

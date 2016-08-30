dc0f859811f18b54f7cc16fab05c76e324b9264f
Heresy <kheresy@gmail.com>
2016-08-30 13:30:17 +0800

    List all symbols

--- App/AppMain.cpp
+++ App/AppMain.cpp
@@ -16,20 +16,28 @@ int main(int argc, char** argv)
 	boost::dll::library_info infDLL(pathDLL);
 
 	// get all section name
+	std::cout << "List sections\n";
 	std::vector<std::string> vSections = infDLL.sections();
 	for (const std::string& sSection : vSections)
 	{
-		std::cout << "Section: " << sSection << "\n";
-
 		if (sSection[0] == '.')
 			continue;
 
+		std::cout << " Section: " << sSection << "\n";
+
 		// get all symbol name
-		std::vector<std::string> vSymbol = infDLL.symbols(sSection);
-		for (const std::string& sSymbol : vSymbol)
+		std::vector<std::string> vSymbols = infDLL.symbols(sSection);
+		for (const std::string& sSymbol : vSymbols)
 		{
-			std::cout << " - " << sSymbol << "\n";
+			std::cout << "  - " << sSymbol << "\n";
 		}
+	}
+
+	std::cout << "\nList all symbols\n";
+	std::vector<std::string> vSymbols = infDLL.symbols();
+	for (const std::string& sSymbol : vSymbols)
+	{
+		std::cout << " " << sSymbol << "\n";
 	}
 
 	return 0;

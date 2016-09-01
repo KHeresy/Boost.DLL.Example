e04be21dd6e622527ebf9b2545b959dd7124bcd5
Heresy <kheresy@gmail.com>
2016-09-01 09:16:27 +0800

    Modify GetModule()

--- App/AppMain.cpp
+++ App/AppMain.cpp
@@ -8,23 +8,25 @@
 
 struct library_holding_deleter
 {
-	library_holding_deleter(const boost::filesystem::path& pathDLL)
-	{
-		mLib = boost::make_shared<boost::dll::shared_library>(pathDLL);
-	}
-
-	boost::shared_ptr<boost::dll::shared_library> mLib;
+	library_holding_deleter(boost::shared_ptr<boost::dll::shared_library> libDLL) : mLib(libDLL){}
 
 	void operator()(CMyAPI* p) const {
 		delete p;
 	}
+
+	boost::shared_ptr<boost::dll::shared_library> mLib;
 };
 
 boost::shared_ptr<CMyAPI> GetModule( const boost::filesystem::path& pathDLL )
 {
-	std::function<API_Create> funcExt = boost::dll::import_alias<API_Create>(pathDLL, "create_plugin");
-	CMyAPI* pModule = funcExt();
-	return boost::shared_ptr<CMyAPI>(pModule,library_holding_deleter(pathDLL));
+	boost::shared_ptr<boost::dll::shared_library> mLib = boost::make_shared<boost::dll::shared_library>(pathDLL);
+	if (mLib && mLib->has("create_plugin"))
+	{
+		std::function<API_Create> funcCreate = mLib->get_alias<API_Create>("create_plugin");
+		CMyAPI* pModule = funcCreate();
+		return boost::shared_ptr<CMyAPI>(pModule, library_holding_deleter(mLib));
+	}
+	return nullptr;
 }
 
 int main(int argc, char** argv)
@@ -44,7 +46,10 @@ int main(int argc, char** argv)
 				std::cout << "\nTry to load " << pathFile << std::endl;
 
 				boost::shared_ptr<CMyAPI> pModule = GetModule(pathFile);
-				std::cout << "Module [" << pModule->getName() << "], result: " << pModule->compute(vData) << std::endl;
+				if (pModule)
+				{
+					std::cout << "Module [" << pModule->getName() << "], result: " << pModule->compute(vData) << std::endl;
+				}
 			}
 		}
 	}
